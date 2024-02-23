9b4b472e3922bccfee90f769c04c30acf7557ac4
Sai Prathap <saiprathapkasireddy@gmail.com>
2024-02-22 20:05:51 -0500

    details of countries

---
+++ data.js
@@ -0,0 +1,78 @@
+let anotherDataSet = [
+    {
+      city: "New York",
+      population: 8398748,
+      attractions: ["Central Park", "Times Square", "Statue of Liberty"],
+      country: "United States",
+      famousFood: ["New York-style pizza", "Bagels"],
+      publicTransport: ["Subway", "Bus"],
+    },
+    {
+      city: "Tokyo",
+      population: 37393129,
+      attractions: ["Tokyo Tower", "Shibuya Crossing", "Senso-ji Temple"],
+      country: "Japan",
+      famousFood: ["Sushi", "Ramen"],
+      publicTransport: ["Tokyo Metro", "JR East"],
+    },
+    {
+      city: "Paris",
+      population: 2140526,
+      attractions: ["Eiffel Tower", "Louvre Museum", "Notre-Dame Cathedral"],
+      country: "France",
+      famousFood: ["Croissant", "Escargot"],
+      publicTransport: ["Metro", "Bus"],
+    },
+    {
+      city: "Sydney",
+      population: 5312163,
+      attractions: ["Sydney Opera House", "Bondi Beach", "Royal Botanic Garden"],
+      country: "Australia",
+      famousFood: ["Pavlova", "Vegemite"],
+      publicTransport: ["Train", "Ferry"],
+    },
+    {
+      city: "Cairo",
+      population: 10425760,
+      attractions: [
+        "Pyramids of Giza",
+        "Egyptian Museum",
+        "Khan El Khalili Bazaar",
+      ],
+      country: "Egypt",
+      famousFood: ["Koshari", "Falafel"],
+      publicTransport: ["Metro", "Microbus"],
+    },
+    {
+      city: "London",
+      population: 8982000,
+      attractions: ["British Museum", "Buckingham Palace", "Tower Bridge"],
+      country: "United Kingdom",
+      famousFood: ["Fish and Chips", "Full English Breakfast"],
+      publicTransport: ["London Underground", "Double-decker Bus"],
+    },
+    {
+      city: "Los Angeles",
+      population: 3999759,
+      attractions: [
+        "Hollywood Walk of Fame",
+        "Santa Monica Pier",
+        "Getty Center",
+      ],
+      country: "United States",
+      famousFood: ["In-N-Out Burger", "Tacos"],
+      publicTransport: ["Metro Rail", "Bus"],
+    },
+    {
+      city: "Beijing",
+      population: 21542000,
+      attractions: ["Great Wall of China", "Forbidden City", "Summer Palace"],
+      country: "China",
+      famousFood: ["Peking Duck", "Dim Sum"],
+      publicTransport: ["Beijing Subway", "Bus"],
+    },
+  ];
+
+
+module.exports=anotherDataSet;
+  
